7465e6c7228232c6ea0ebff67e72100e2ac5baf9
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-08-23 10:33:34 +0530

    add last in LL

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,11 +1,11 @@
 package LinkedList;
 
 public class LinkedList {
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
@@ -14,11 +14,11 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data){
-        // step1 = create newNode 
+    public void addFirst(int data) {
+        // step1 = create newNode
         Node newNode = new Node(data);
 
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -26,13 +26,25 @@ public class LinkedList {
         // step2 - newNode's next = head
         newNode.next = head;
 
-        //step3 - head = newNode
+        // step3 - head = newNode
         head = newNode;
     }
 
-    public static void main(String args[]){
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = null;
+        tail = newNode;
+    }
+
+    public static void main(String args[]) {
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
         ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
     }
 }

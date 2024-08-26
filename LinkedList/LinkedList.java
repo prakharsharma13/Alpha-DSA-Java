ffa869bbd6e2dee673018512ef14a3e5263b582c
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-08-26 12:28:44 +0530

    remove first in LinkedList

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -76,6 +76,22 @@ public class LinkedList {
         temp.next = newNode;
     }
 
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty!");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
@@ -84,6 +100,8 @@ public class LinkedList {
         ll.addLast(5);
         ll.add(2, 3);
         ll.print();
-        System.out.println(ll.size);
+        // System.out.println(ll.size);
+        ll.removeFirst();
+        ll.print();
     }
 }

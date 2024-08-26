c601781358f47d99e68c054db6d2d9a3c8a937b0
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-08-26 17:27:55 +0530

    remove last from Linked List

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -92,6 +92,27 @@ public class LinkedList {
         return val;
     }
 
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("LL is empty!");
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev : i = size-2;
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
@@ -103,5 +124,8 @@ public class LinkedList {
         // System.out.println(ll.size);
         ll.removeFirst();
         ll.print();
+        ll.removeLast();
+        ll.print();
+        System.out.println(ll.size);
     }
 }

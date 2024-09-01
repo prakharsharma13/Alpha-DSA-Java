6b1b1fc463516f180a923afc17977e7f24ed570f
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-01 12:09:06 +0530

    Selection Sort

--- BasicSorting/BasicSorting.java
+++ BasicSorting/BasicSorting.java
@@ -20,9 +20,26 @@ public class BasicSorting {
         }
         System.out.println();
     } 
+
+    public static void selectionSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++ ){
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        selectionSort(arr);
         printArr(arr);
     }
 }

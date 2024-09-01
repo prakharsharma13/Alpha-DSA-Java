08df7b7b092f9d99a2c86b8877e1dcbd473864f2
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-01 12:47:47 +0530

    Inbuilt Sort

--- BasicSorting/BasicSorting.java
+++ BasicSorting/BasicSorting.java
@@ -1,4 +1,6 @@
 package BasicSorting;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class BasicSorting {
     public static void bubbleSort(int arr[]){
@@ -14,7 +16,7 @@ public class BasicSorting {
         }
     }
 
-    public static void printArr(int arr[]){
+    public static void printArr(Integer arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }
@@ -51,10 +53,12 @@ public class BasicSorting {
         }
     }
     public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2};
+        Integer arr[] = {5,4,1,3,2};
         // bubbleSort(arr);
         // selectionSort(arr); 
-        insertionSort(arr);
+        // insertionSort(arr);
+        Arrays.sort(arr,0 ,3);
+        Arrays.sort(arr, Collections.reverseOrder());
         printArr(arr);
     }
 }

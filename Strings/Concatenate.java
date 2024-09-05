67454a4c7d6deecddc3ccc1662722743dd1302cf
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-05 09:35:24 +0530

    String concatenation

---
+++ Strings/Concatenate.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Concatenate {
+    public static void main(String[] args) {
+        String firstName = "Prakhar";
+        String lastName = "Sharma";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}

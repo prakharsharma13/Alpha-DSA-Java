1ac1ba3797a03d9fe49d4d5a1de8ba76035e882a
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-05 09:24:25 +0530

    string initialization

---
+++ Strings/Strings.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        // STRINGS ARE IMMUTABLE 
+    }
+}

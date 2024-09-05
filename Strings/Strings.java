283972c38974696d3161ee01acdfbf2416f5559b
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-05 09:28:48 +0530

    Input/Output

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -1,10 +1,17 @@
 package Strings;
 
+import java.util.*;
+
 public class Strings {
     public static void main(String[] args) {
         String str = "abcd";
         String str2 = new String("xyz");
 
         // STRINGS ARE IMMUTABLE 
+
+        Scanner sc= new Scanner(System.in);
+        String name;
+        name = sc.nextLine(); //next only takes single word without any line change or space but nextLine takes the whole line 
+        System.out.println(name);
     }
 }

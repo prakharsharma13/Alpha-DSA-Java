90ec32fc2413365dc94a84f124b19e365ca623b8
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-13 09:49:28 +0530

    first occurence

---
+++ RecursionBasics/FirstOccurence.java
@@ -0,0 +1,18 @@
+package RecursionBasics;
+
+public class FirstOccurence {
+    public static int firstOccur(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        if (i == key) {
+            return i;
+        }
+        return firstOccur(arr, key, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 5, 6, 5, 8 };
+        System.out.println(firstOccur(arr, 5, 0));
+    }
+}

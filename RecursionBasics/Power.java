a4151df744d4ec2767714b94f105019d2a18227c
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-14 09:40:18 +0530

    print x to the power n (optimized)

--- RecursionBasics/Power.java
+++ RecursionBasics/Power.java
@@ -1,6 +1,19 @@
 package RecursionBasics;
 
 public class Power {
+    public static int optimizedPower(int a, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int halfPowerSq = optimizedPower(a, n / 2) * optimizedPower(a, n / 2);
+
+        if (n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+
+        return halfPowerSq;
+    }
+
     public static int power(int x, int n) {
         if (n == 0) {
             return 1;
@@ -11,6 +24,8 @@ public class Power {
     public static void main(String[] args) {
         int x = 2;
         int n = 10;
+        int a = 2;
         System.out.println(power(x, n));
+        System.out.println(optimizedPower(a, n));
     }
 }

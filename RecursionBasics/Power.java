910ad5ba04f3aa966d22deff5a0821f17053c64b
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-14 08:33:08 +0530

    print x to the power n

---
+++ RecursionBasics/Power.java
@@ -0,0 +1,16 @@
+package RecursionBasics;
+
+public class Power {
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * power(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 10;
+        System.out.println(power(x, n));
+    }
+}
